bde71a2d5525ac8bf523640b72ffe2026977d75f
Nathália Martins Caldonho <82842237+NathyM011@users.noreply.github.com>
2023-10-19 16:35:49 +0000

    alteração formulário de contato

--- envia.php
+++ envia.php
@@ -1,28 +1,4 @@
 <?php
-
-    $nome = addslashes($_POST['nome']);
-    $email = addslashes($_POST['email']);
-    $telefone = addslashes($_POST['telefone']);
-    $mensagem = addslashes($_POST['mensagem']);
-
-    $destinatario = "contato.nathaliacaldonho@gmail.com";
-    $assunto = "Coleta de dados - NMC Web - Development";
-
-    $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$telefone."\n"."Mensagem: ".$mensagem;
-
-    $cabecalho = "From: contato.nathaliacaldonho@gmail.com "."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
-
-    if(mail($destinatario, $assunto, $corpo, $cabecalho)){
-        echo("Mensagem enviada com sucesso!");
-    }
-    else{
-        echo("Houve um erro ao enviar o email!");
-    }
-
-
-?>
-
-<?php
 if (isset($_POST['btn-enviar'])) {
  
  //Variaveis de POST, Alterar somente se necessário 
