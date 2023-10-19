0baf1faa8301b6e922f2e0982830b5ddea9b5a8a
Nathália Martins Caldonho <82842237+NathyM011@users.noreply.github.com>
2023-10-19 16:50:22 +0000

    alteração no formulário de contato

--- envia.php
+++ envia.php
@@ -1,45 +1,27 @@
 <?php
-if (isset($_POST['btn-enviar'])) {
- 
- //Variaveis de POST, Alterar somente se necessário 
- //====================================================
- $nome = $_POST['nome'];
- $email = $_POST['email'];
- $telefone = $_POST['telefone']; 
- $mensagem = $_POST['mensagem'];
- //====================================================
- 
- //REMETENTE --> ESTE EMAIL TEM QUE SER VALIDO DO DOMINIO
- //==================================================== 
- $email_remetente = "nathaliacaldonho@gmail.com "; // deve ser uma conta de email do seu dominio 
- //====================================================
- 
- //Configurações do email, ajustar conforme necessidade
- //==================================================== 
- $email_destinatario = "contato.nathaliacaldonho@gmail.com"; // pode ser qualquer email que receberá as mensagens
- $email_reply = "$email"; 
- $email_assunto = "Coleta de dados - NMC Web - Development"; // Este será o assunto da mensagem
- //====================================================
- 
- //Monta o Corpo da Mensagem
- //====================================================
- $email_conteudo = "Nome = $nome \n"; 
- $email_conteudo .= "Email = $email \n";
- $email_conteudo .= "Telefone = $telefone \n"; 
- $email_conteudo .= "Mensagem = $mensagem \n"; 
- //====================================================
- 
- //Seta os Headers (Alterar somente caso necessario) 
- //==================================================== 
- $email_headers = implode ( "\n",array ( "From: $email_remetente", "Reply-To: $email_reply", "Return-Path: $email_remetente","MIME-Version: 1.0","X-Priority: 3","Content-Type: text/html; charset=UTF-8" ) );
- //====================================================
- 
- //Enviando o email 
- //==================================================== 
- if (mail ($email_destinatario, $email_assunto, nl2br($email_conteudo), $email_headers)){ 
- echo "</b>E-Mail enviado com sucesso!</b>"; 
- } 
- else{ 
- echo "</b>Falha no envio do E-Mail!</b>"; } 
- //====================================================
-} 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $telefone = $_POST["telefone"];
+    $mensagem = $_POST["mensagem"];
+
+    // Configurações de email
+    $destinatario = "contato.nathaliacaldonho@gmail.com"; // Substitua pelo seu endereço de email
+    $assunto = "Nova mensagem do formulário de contato - NMC - DEV";
+
+    // Monta o corpo da mensagem
+    $mensagem_formatada = "Nome: $nome\n";
+    $mensagem_formatada .= "Email: $email\n";
+    $mensagem_formatada .= "Telefone: $telefone\n\n";
+    $mensagem_formatada .= "Mensagem:\n$mensagem";
+
+    // Envia o email
+    if (mail($destinatario, $assunto, $mensagem_formatada)) {
+        echo "Mensagem enviada com sucesso!";
+    } else {
+        echo "Houve um erro ao enviar a mensagem. Por favor, tente novamente mais tarde.";
+    }
+} else {
+    echo "Acesso inválido.";
+}
+?>
